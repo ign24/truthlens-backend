315cd8b74fd7d1b281396088dbeadc7a1a2fbda9
Ignacio Zúñiga Navarro <nnznn-@hotmail.com>
2025-05-19 19:39:25 -0300

    Enable CORS for Netlify domain final

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def create_app() -> FastAPI:
     # Configure CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173","https://papaya-bavarois-773352.netlify.app/"],  # Frontend URL
+        allow_origins=["http://localhost:5173","https://papaya-bavarois-773352.netlify.app"],  # Frontend URL
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
